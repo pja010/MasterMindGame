5dfb3afeb8461789c37617640859c6cfca6276a8
Guillermo Torres <glt005@bucknell.edu>
2020-10-12 09:20:36 -0400

    Initializing new project

---
+++ README.md
@@ -0,0 +1,13 @@
+# CSCI 205 - Software Engineering and Design
+Bucknell University
+Lewisburg, PA
+
+### Course Info
+Instructor: name here
+Semester: Enter the semester here
+## Team Information
+List team members
+## Project Information
+*Write a few sentences about your project*
+## How to run it
+*Update this with information about how to build and run your project*
---
+++ src/main/java/main/Main.java
@@ -0,0 +1,25 @@
+/* *****************************************
+ * CSCI205 - Software Engineering and Design
+ * Fall 2020
+ * Instructor: Prof. Brian King
+ *
+ * Name: Guillermo Torres and Per Ästrom
+ * Section: 02-8:50 am
+ * Date: 10/12/20
+ * Time: 9:08 AM
+ *
+ * Project: csci205_hw
+ * Package: main
+ * Class: Main
+ *
+ * Description:
+ *
+ * ****************************************
+ */
+package main;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
